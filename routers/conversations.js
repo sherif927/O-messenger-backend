288eaed246da61683fe1099553d408a1812767a0
AbdelRahman Tarek <abdelrahman.tare.mahdy@gmail.com>
2018-06-11 21:32:46 +0200

    chatting

--- routers/conversations.js
+++ routers/conversations.js
@@ -6,15 +6,17 @@ const _ = require('lodash');
 const authenticate = require('../middleware/authentication/authenticate');
 var router = express.Router();
 
-router.post('/newConversation',async(req,res)=>{
+router.post('/newConversation', async (req, res) => {
   try {
     var body = _.pick(req.body, ['users']);
     console.log(body.users);
-    var conversation=new Conversation({ users:body.users });
+    console.log(body);
+    var conversation = new Conversation({ users: body.users });
     await conversation.save();
     res.status(200).send(conversation);
   } catch (e) {
     res.status(400).send(e);
+    console.log(e);
   }
 });
 
